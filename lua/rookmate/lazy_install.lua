11e6dc1f7193fbf9e44c770a525d28c4a19c0663
Francisco Esteves <francisco.esteves@mov.ai>
2024-08-25 21:46:07 +0100

    Add lazy UI icons

--- lua/rookmate/lazy_install.lua
+++ lua/rookmate/lazy_install.lua
@@ -10,7 +10,27 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup {
+require('lazy').setup({
   spec = 'rookmate.lazy',
   change_detection = { notify = false },
-}
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
